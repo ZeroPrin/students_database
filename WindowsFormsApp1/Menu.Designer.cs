e56df65299ec578131c937fb9b7d53b5f9873907
Глеб Панченко <panchenko.glebka@gmail.com>
2022-05-26 16:49:43 +0500

    Готово

--- WindowsFormsApp1/Menu.Designer.cs
+++ WindowsFormsApp1/Menu.Designer.cs
@@ -28,63 +28,326 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.check_but = new System.Windows.Forms.Button();
-            this.add_but = new System.Windows.Forms.Button();
-            this.show_list_but = new System.Windows.Forms.Button();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Menu));
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.check_pass = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textbox_name = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textbox_surname = new System.Windows.Forms.TextBox();
+            this.textbox_patronymic = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textbox_pass = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.textbox_group = new System.Windows.Forms.TextBox();
+            this.textbox_year = new System.Windows.Forms.TextBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.button4 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
-            // check_but
+            // listView1
             // 
-            this.check_but.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.check_but.Location = new System.Drawing.Point(118, 126);
-            this.check_but.Name = "check_but";
-            this.check_but.Size = new System.Drawing.Size(131, 52);
-            this.check_but.TabIndex = 3;
-            this.check_but.Text = "Проверка";
-            this.check_but.UseVisualStyleBackColor = true;
-            this.check_but.Click += new System.EventHandler(this.check_but_Click);
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(12, 41);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(776, 426);
+            this.listView1.TabIndex = 6;
+            this.listView1.UseCompatibleStateImageBehavior = false;
             // 
-            // add_but
+            // button1
             // 
-            this.add_but.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.add_but.Location = new System.Drawing.Point(118, 184);
-            this.add_but.Name = "add_but";
-            this.add_but.Size = new System.Drawing.Size(131, 52);
-            this.add_but.TabIndex = 4;
-            this.add_but.Text = "Добавление";
-            this.add_but.UseVisualStyleBackColor = true;
-            this.add_but.Click += new System.EventHandler(this.add_but_Click);
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Обновить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // show_list_but
+            // check_pass
             // 
-            this.show_list_but.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.show_list_but.Location = new System.Drawing.Point(118, 242);
-            this.show_list_but.Name = "show_list_but";
-            this.show_list_but.Size = new System.Drawing.Size(131, 52);
-            this.show_list_but.TabIndex = 5;
-            this.show_list_but.Text = "Таблица";
-            this.show_list_but.UseVisualStyleBackColor = true;
-            this.show_list_but.Click += new System.EventHandler(this.show_list_but_Click);
+            this.check_pass.Location = new System.Drawing.Point(928, 75);
+            this.check_pass.Name = "check_pass";
+            this.check_pass.Size = new System.Drawing.Size(100, 20);
+            this.check_pass.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(830, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Проверить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textbox_name
+            // 
+            this.textbox_name.Location = new System.Drawing.Point(830, 203);
+            this.textbox_name.Name = "textbox_name";
+            this.textbox_name.Size = new System.Drawing.Size(100, 20);
+            this.textbox_name.TabIndex = 11;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(830, 147);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Добавить";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textbox_surname
+            // 
+            this.textbox_surname.Location = new System.Drawing.Point(936, 203);
+            this.textbox_surname.Name = "textbox_surname";
+            this.textbox_surname.Size = new System.Drawing.Size(100, 20);
+            this.textbox_surname.TabIndex = 12;
+            // 
+            // textbox_patronymic
+            // 
+            this.textbox_patronymic.Location = new System.Drawing.Point(1042, 203);
+            this.textbox_patronymic.Name = "textbox_patronymic";
+            this.textbox_patronymic.Size = new System.Drawing.Size(100, 20);
+            this.textbox_patronymic.TabIndex = 13;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(838, 77);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(67, 18);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Паспорт";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(827, 182);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 18);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "Имя";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(933, 182);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 18);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Фамилия";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(1039, 182);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 18);
+            this.label4.TabIndex = 21;
+            this.label4.Text = "Отчество";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(827, 236);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(116, 18);
+            this.label5.TabIndex = 23;
+            this.label5.Text = "Дата рождения";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(1011, 236);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 18);
+            this.label6.TabIndex = 27;
+            this.label6.Text = "Паспорт";
+            // 
+            // textbox_pass
+            // 
+            this.textbox_pass.Location = new System.Drawing.Point(1014, 257);
+            this.textbox_pass.Name = "textbox_pass";
+            this.textbox_pass.Size = new System.Drawing.Size(100, 20);
+            this.textbox_pass.TabIndex = 26;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(903, 297);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 18);
+            this.label7.TabIndex = 31;
+            this.label7.Text = "Группа";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(827, 297);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(41, 18);
+            this.label8.TabIndex = 30;
+            this.label8.Text = "Курс";
+            // 
+            // textbox_group
+            // 
+            this.textbox_group.Location = new System.Drawing.Point(906, 318);
+            this.textbox_group.Name = "textbox_group";
+            this.textbox_group.Size = new System.Drawing.Size(71, 20);
+            this.textbox_group.TabIndex = 29;
+            // 
+            // textbox_year
+            // 
+            this.textbox_year.Location = new System.Drawing.Point(830, 318);
+            this.textbox_year.Name = "textbox_year";
+            this.textbox_year.Size = new System.Drawing.Size(45, 20);
+            this.textbox_year.TabIndex = 28;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(830, 257);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(45, 20);
+            this.numericUpDown1.TabIndex = 32;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Location = new System.Drawing.Point(881, 257);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(45, 20);
+            this.numericUpDown2.TabIndex = 33;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Location = new System.Drawing.Point(932, 257);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(45, 20);
+            this.numericUpDown3.TabIndex = 34;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(93, 12);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 35;
+            this.button4.Text = "Удалить";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(1075, 236);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(16, 18);
+            this.pictureBox1.TabIndex = 36;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.Location = new System.Drawing.Point(906, 77);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(16, 18);
+            this.pictureBox2.TabIndex = 37;
+            this.pictureBox2.TabStop = false;
             // 
             // Menu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonShadow;
-            this.ClientSize = new System.Drawing.Size(407, 426);
-            this.Controls.Add(this.show_list_but);
-            this.Controls.Add(this.add_but);
-            this.Controls.Add(this.check_but);
+            this.ClientSize = new System.Drawing.Size(1157, 499);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textbox_group);
+            this.Controls.Add(this.textbox_year);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textbox_pass);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textbox_patronymic);
+            this.Controls.Add(this.textbox_surname);
+            this.Controls.Add(this.textbox_name);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.check_pass);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listView1);
             this.Name = "Menu";
-            this.Text = "Form1";
+            this.Text = "База студентов";
+            this.Load += new System.EventHandler(this.Menu_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-        private System.Windows.Forms.Button check_but;
-        private System.Windows.Forms.Button add_but;
-        private System.Windows.Forms.Button show_list_but;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox check_pass;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textbox_name;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textbox_surname;
+        private System.Windows.Forms.TextBox textbox_patronymic;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textbox_pass;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox textbox_group;
+        private System.Windows.Forms.TextBox textbox_year;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
     }
 }
 
--- WindowsFormsApp1/Menu.cs
+++ WindowsFormsApp1/Menu.cs
@@ -18,22 +18,212 @@ namespace WindowsFormsApp1
             InitializeComponent();
         }
 
-        private void check_but_Click(object sender, EventArgs e)
+        private void Menu_Load(object sender, EventArgs e)
         {
-            Check_student cst = new Check_student();
-            cst.Show();
+            ToolTip toolTip1 = new ToolTip();
+
+            toolTip1.AutoPopDelay = 5000;
+            toolTip1.InitialDelay = 1000;
+            toolTip1.ReshowDelay = 500;
+            
+            toolTip1.ShowAlways = true;
+
+            toolTip1.SetToolTip(this.pictureBox1, "Необходимо ввести серию и номер без пробелов");
+            toolTip1.SetToolTip(this.pictureBox2, "Необходимо ввести серию и номер без пробелов");
+
+
+            listView1.GridLines = true;
+
+            listView1.FullRowSelect = true;
+
+            listView1.View = View.Details;
+
+            listView1.Columns.Add("");
+            listView1.Columns.Add("Id");
+            listView1.Columns.Add("Имя");
+            listView1.Columns.Add("Фамилия");
+            listView1.Columns.Add("Отчество");
+            listView1.Columns.Add("Дата рождения");
+            listView1.Columns.Add("Паспорт");
+            listView1.Columns.Add("Курс");
+            listView1.Columns.Add("Группа");
+
+            DB db = new DB();
+
+            DataTable table = new DataTable();
+
+            db.open_connection();
+
+            using (db.get_connection())
+            {
+                MySqlCommand command = new MySqlCommand("SELECT * FROM `students_bkt`", db.get_connection());
+
+                MySqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        ListViewItem item = new ListViewItem(new string[]
+                        {
+                        Convert.ToString(listView1.Items.Count+1),
+                        Convert.ToString(reader.GetString("id")),
+                        Convert.ToString(reader.GetString("name")),
+                        Convert.ToString(reader.GetString("surname")),
+                        Convert.ToString(reader.GetString("patronymic")),
+                        Convert.ToString(reader.GetString("birthday")),
+                        Convert.ToString(reader.GetString("passport")),
+                        Convert.ToString(reader.GetString("year")),
+                        Convert.ToString(reader.GetString("n_group")),
+                        });
+                        listView1.Items.Add(item);
+                    }
+                }
+                reader.Close();
+
+            }
+
+            db.close_connection();
+
         }
 
-        private void add_but_Click(object sender, EventArgs e)
+        private void button1_Click(object sender, EventArgs e)
         {
-            Add_student ast = new Add_student();
-            ast.Show();
+            listView1.Items.Clear();
+
+            DB db = new DB();
+
+            DataTable table = new DataTable();
+
+            db.open_connection();
+
+            using (db.get_connection())
+            {
+                MySqlCommand command = new MySqlCommand("SELECT * FROM `students_bkt`", db.get_connection());
+
+                MySqlDataReader reader = command.ExecuteReader();
+
+                if (reader.HasRows)
+                {
+                    while (reader.Read())
+                    {
+                        ListViewItem item = new ListViewItem(new string[]
+                        {
+                        Convert.ToString(listView1.Items.Count+1),
+                        Convert.ToString(reader.GetString("id")),
+                        Convert.ToString(reader.GetString("name")),
+                        Convert.ToString(reader.GetString("surname")),
+                        Convert.ToString(reader.GetString("patronymic")),
+                        Convert.ToString(reader.GetString("birthday")),
+                        Convert.ToString(reader.GetString("passport")),
+                        Convert.ToString(reader.GetString("year")),
+                        Convert.ToString(reader.GetString("n_group")),
+                        });
+                        listView1.Items.Add(item);
+                    }
+                }
+                reader.Close();
+
+            }
+
+            db.close_connection();
         }
 
-        private void show_list_but_Click(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
-            Show_list_students sls = new Show_list_students();
-            sls.Show();
+            string pass = check_pass.Text;
+
+            DB db = new DB();
+
+            DataTable table = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `students_bkt` WHERE `passport` = @pass", db.get_connection());
+            command.Parameters.Add("@pass", MySqlDbType.VarChar).Value = pass;
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count > 0)
+                MessageBox.Show("всё чики пуки");
+            else
+                MessageBox.Show("браза... капут...");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            if (check_student() == true)
+            {
+                MessageBox.Show("уже с нами");
+                return;
+            }
+
+            DB db = new DB();
+
+            MySqlCommand command = new MySqlCommand("INSERT INTO `students_bkt` (`name`, `surname`, `patronymic`, `birthday`, `passport`, `year`, `n_group`) VALUES (@name, @surname, @patronymic, @birthday, @passport, @year, @n_group);", db.get_connection());
+
+            string birthday = numericUpDown1.Value.ToString() + numericUpDown2.Value.ToString() + numericUpDown3.Value.ToString();
+
+            command.Parameters.Add("@name", MySqlDbType.VarChar).Value = textbox_name.Text;
+            command.Parameters.Add("@surname", MySqlDbType.VarChar).Value = textbox_surname.Text;
+            command.Parameters.Add("@patronymic", MySqlDbType.VarChar).Value = textbox_patronymic.Text;
+            command.Parameters.Add("@birthday", MySqlDbType.VarChar).Value = birthday;
+            command.Parameters.Add("@passport", MySqlDbType.VarChar).Value = textbox_pass.Text;
+            command.Parameters.Add("@year", MySqlDbType.VarChar).Value = textbox_year.Text;
+            command.Parameters.Add("@n_group", MySqlDbType.VarChar).Value = textbox_group.Text;
+
+            db.open_connection();
+
+            if (command.ExecuteNonQuery() == 1)
+            {
+                MessageBox.Show("добавил братан");
+                button1_Click(sender, e);
+            }
+            else
+                MessageBox.Show("чёт не пошло");
+
+            db.close_connection();
+        }
+
+        public Boolean check_student()
+        {
+            DB db = new DB();
+
+            DataTable table = new DataTable();
+
+            MySqlDataAdapter adapter = new MySqlDataAdapter();
+
+            MySqlCommand command = new MySqlCommand("SELECT * FROM `students_bkt` WHERE `passport` = @pass", db.get_connection());
+            command.Parameters.Add("@pass", MySqlDbType.VarChar).Value = textbox_pass.Text;
+
+            adapter.SelectCommand = command;
+            adapter.Fill(table);
+
+            if (table.Rows.Count > 0)
+                return true;
+            else
+                return false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            DB db = new DB();
+
+            MySqlCommand delete = new MySqlCommand($"DELETE FROM `students_bkt` WHERE id = {listView1.SelectedItems[0].SubItems[1].Text}", db.get_connection());
+
+            db.open_connection();
+
+            if (delete.ExecuteNonQuery() == 1)
+            {
+                MessageBox.Show("уволен");
+                button1_Click(sender, e);
+            }
+            else
+                MessageBox.Show("ладно, живи");
+
+            db.close_connection();
         }
     }
 }
